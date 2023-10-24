550d437ac94ede8f9be50045a9f468ca8705954d
Eliot Lear <github@ofcourseimright.com>
2023-10-24 08:48:38 +0200

    Revert "Remove keys; write a script that generates an appropriate CA cert. (#4)" (#9)
    This reverts commit 3b69d33d8b7eb9754d8e6276e9ea71809a0736d4.

--- gateway/certs/gen_cert.sh
+++ gateway/certs/gen_cert.sh
@@ -2,8 +2,10 @@
 
 name=$1
 
-openssl req -nodes -new -keyout $name.key -out $name.csr -subj "/C=US/O=TieDie Test Cert/OU=Test.$$/CN=$name"
+openssl req -nodes -new -keyout $name.key -out $name.csr -subj "/C=US/ST=CA/L=San Jose/O=My Company/OU=My Department/CN=$name"
 openssl x509 -req -days 365 -in $name.csr -CA ../ca_certificates/ca.pem -CAkey ../ca_certificates/ca.key -CAcreateserial -out $name.crt                              
 openssl pkcs12 -export -out $name.p12 -inkey $name.key -in $name.crt        
 
 rm $name.csr
+rm $name.key
+rm $name.crt
