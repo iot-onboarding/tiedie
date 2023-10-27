cf46fdabcf569ebf62595d408e6e34c1a9c70ec7
Rohit Mohan <rohitmo@cisco.com>
2023-10-27 12:14:59 -0700

    Added missing licenses

--- java-sdk/sdk/src/main/java/com/cisco/tiedie/dto/control/ble/BleCharacteristic.java
+++ java-sdk/sdk/src/main/java/com/cisco/tiedie/dto/control/ble/BleCharacteristic.java
@@ -1,3 +1,8 @@
+// Copyright (c) 2023, Cisco and/or its affiliates.
+// All rights reserved.
+// See LICENSE file in this distribution.
+// SPDX-License-Identifier: Apache-2.0
+
 package com.cisco.tiedie.dto.control.ble;
 
 import java.util.List;
--- java-sdk/sdk/src/main/java/com/cisco/tiedie/dto/control/ble/BleDescriptors.java
+++ java-sdk/sdk/src/main/java/com/cisco/tiedie/dto/control/ble/BleDescriptors.java
@@ -1,3 +1,8 @@
+// Copyright (c) 2023, Cisco and/or its affiliates.
+// All rights reserved.
+// See LICENSE file in this distribution.
+// SPDX-License-Identifier: Apache-2.0
+
 package com.cisco.tiedie.dto.control.ble;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
